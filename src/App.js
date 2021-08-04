e50ed477973f710ffa5015b10fc8a87f8fc79b70
Shahrukh170C <sk902224@gmail.com>
2021-08-04 09:48:51 +0500

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="border-4 h-screen overflow-hidden border-green-300 flex flex-row">
+      <div className="border-2 h-full  w-3/12 border-green-700 p-5">
+      </div>
+      <div className="border-2 overflow-y-auto w-9/12 bg-red-500 p-7">B
+     
+      </div>
     </div>
   );
 }
